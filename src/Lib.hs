699f529a8d496c003a5be869ccadbec6fd1ee704
Jeong Yonguk <ywj1022@gmail.com>
2016-10-07 02:51:29 +0900

    Fetch remote

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-    ( someFunc
+    ( fetch
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Text.RSS.Types
+import Text.RSS.Conduit.Parse
+import Network.Wreq
+import Control.Lens ((^.))
+import Data.ByteString.Lazy (toStrict)
+import qualified Data.ByteString.Char8 as DBC
+import qualified Data.List.Split as DLS
+
+fetch :: String -> IO String
+fetch s = do
+  r <- get s
+  return $ DBC.unpack $ toStrict $ r ^. responseBody
+
+splitByNewLine :: String -> [String]
+splitByNewLine s = DLS.splitOn "\n" s
