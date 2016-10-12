1f0f6e806087c23c9c5e60e5e9550825aec9625a
Jun <me@noraesae.net>
2016-10-13 00:44:20 +0900

    Add filtering to queryAnimeList

--- src/Nyaa.hs
+++ src/Nyaa.hs
@@ -3,64 +3,63 @@
 module Nyaa
   ( fetch
   , fetchAndParseRSS
-  , findItems
   , queryAnimeList
   ) where
 
-import Text.RSS.Types
-import Text.RSS.Conduit.Parse
-import Network.Wreq
-import Control.Lens ((^.))
+import Anime
+import Control.Lens ((^.), (&), (.~))
+import Control.Monad (liftM2)
 import Data.ByteString.Lazy (toStrict)
-import Data.UnixTime (UnixTime, parseUnixTime, mailDateFormat)
-import Data.List.Split (splitOn)
+import Data.Conduit
+import Data.Conduit.Parser
 import Data.List (isInfixOf)
-
+import Data.List.Split (splitOn)
+import Data.Time.Clock (UTCTime)
+import Data.Time.Format (formatTime, defaultTimeLocale)
+import Data.UnixTime
+import Network.Wreq
+import Text.RSS.Conduit.Parse
+import Text.RSS.Types
+import Text.XML.Stream.Parse as XML hiding (choose)
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.Conduit.List as DCL
 import qualified Data.Text as T
 
-import Data.Conduit
-import qualified Data.Conduit.List as DCL
-import Data.Conduit.Parser
+(.&&.) = liftM2 (&&)
 
-import Text.XML.Stream.Parse as XML hiding (choose)
+queryAnimeList :: (UnixTime, UnixTime) -> AnimeQuery -> IO [Anime]
+queryAnimeList range query = do
+  rss <- fetchAndParseRSS query
+  return $ map toAnime $ filter (isRaw .&&. inRange range) (channelItems rss)
 
-import Anime
-
--- parsePubDate: function to parse pubDate field in RSS
---
---   parsePubDate "Mon, 10 Oct 2016 07:23:11 +0000"
---
--- The code above will return UnixTime for the pubDate.
-parsePubDate :: String -> UnixTime
-parsePubDate = parseUnixTime mailDateFormat . BS.pack
-
--- The datetime query range will be in the type of (UnixTime, UnixTime).
--- The code below is just an example. please feel free to modify it.
-queryAnimeList :: (UnixTime, UnixTime) -> String -> IO [Anime]
-queryAnimeList (since, until) animeName = do
-  rss <- fetchAndParseRSS "http://nyaa.se/?page=rss"
-  return $ map toAnime (findItems rss animeName)
-
--- Please remove the verbose comments above after understanding them.
-
-fetch :: String -> IO String
-fetch s = do
-  r <- get s
-  return $ BS.unpack $ toStrict $ r ^. responseBody
+fetch :: AnimeQuery -> IO String
+fetch query = do
+  r <- getWith options "http://nyaa.se/?page=rss"
+  return . BS.unpack .toStrict $ r ^. responseBody
+  where
+    options = defaults
+      & param "page" .~ ["rss"]
+      & param "cats" .~ ["1_11"]
+      & param "cats" .~ [T.pack query]
 
 splitByNewLine :: String -> [String]
 splitByNewLine s = splitOn "\n" s
 
-fetchAndParseRSS :: String -> IO RssDocument
-fetchAndParseRSS url = do
-  s <- fetch url
+fetchAndParseRSS :: AnimeQuery -> IO RssDocument
+fetchAndParseRSS query = do
+  s <- fetch query
   let input = map T.pack (splitByNewLine s)
   r <- DCL.sourceList input =$= XML.parseText' def $$ runConduitParser rssDocument
   return r
 
-findItems :: RssDocument -> String -> [RssItem]
-findItems document query = filter (titleContain query) (channelItems document)
+isRaw :: RssItem -> Bool
+isRaw item = isInfixOf "Raws" (T.unpack $ itemTitle item)
 
-titleContain :: String -> RssItem -> Bool
-titleContain query item = isInfixOf query (T.unpack $ itemTitle item)
+inRange :: (UnixTime, UnixTime) -> RssItem -> Bool
+inRange (since, til) item =
+  case itemPubDate item of
+    Just utc -> let t = timestamp utc in since < t && t < til
+    otherwise -> False
+  where
+    rfc2616 = "%a, %d %b %Y %H:%M:%S GMT"
+    timestamp = parseUnixTimeGMT webDateFormat . BS.pack . formatTime defaultTimeLocale rfc2616
