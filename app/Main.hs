cd4675ca4e46b63b584bae3ed8f1d264d71f2a0e
Jun <me@noraesae.net>
2016-10-12 22:28:19 +0900

    Implement dummy Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,14 @@
 module Main where
 
+import Anime
 import Control.Concurrent
-import Timestamp (getTimestampRange)
-import qualified Line as Line
+import Data.UnixTime
 import Nyaa
+import Timestamp
+import qualified Line as Line
 
 interval :: Int
-interval = 60 * 1000 * 1000 -- 1 min
+interval = 5 * 60 * 1000 * 1000 -- 5 min
 
 main :: IO ()
 main = do
@@ -20,5 +22,21 @@ loop interval body = do
 
 body :: IO ()
 body = do
-  -- timestampRange <- getTimestampRange
-  Line.send $ Line.Image "Nyaa Nyaa Nyaa" "http://i.imgur.com/rqWCyZ2.jpg"
+  range <- getTimestampRange
+  animeQueries <- getAnimeQueryList
+  mapM_ (queryAndNotify range) animeQueries
+
+queryAndNotify :: (UnixTime, UnixTime) -> AnimeQuery -> IO ()
+queryAndNotify range query =
+  -- FIXME: uncomment following lines and remove return ()
+  -- animes <- queryAnimeList range query
+  -- mapM_ notifyAnime animes
+  return ()
+
+-- FIXME: Rename anime as Anime, and replace a2m with whatever you implemented
+notifyAnime :: anime -> IO ()
+notifyAnime = Line.send . a2m
+
+-- FIXME: Remove the following function
+a2m :: anime -> Line.Message
+a2m = undefined
